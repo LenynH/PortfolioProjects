17f8a6605e361220d22249baf3b51074caadd893
Lenyn Hernandez <lenynhernandezp@gmail.com>
2025-06-09 08:48:29 -0400

    Makes further modifications to first crime table of database

--- EU Crime Project/Recorded Crimes Table.sql
+++ EU Crime Project/Recorded Crimes Table.sql
@@ -1,7 +1,9 @@
+-- Initial Database Creation
 CREATE DATABASE eu_crime;
 
 USE eu_crime;
 
+-- Creating the first table
 CREATE TABLE crime_1998_2007 (
 	DATAFLOW VARCHAR(20),
     LAST_UPDATE DATETIME, 
@@ -18,8 +20,10 @@ CREATE TABLE crime_1998_2007 (
 ALTER TABLE crime_1998_2007
 MODIFY COLUMN OBS_VALUE INT UNSIGNED;
 
+-- Checking file upload location for MySQL
 SELECT @@secure_file_priv;
 
+-- Loading in CSV file
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Recorded Crimes 1998-2007.csv'
 INTO TABLE crime_1998_2007
 FIELDS TERMINATED BY ','
@@ -27,6 +31,34 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-SELECT * 
+-- Adjusting the table to show only necessary information
+ALTER TABLE crime_1998_2007 
+DROP COLUMN LAST_UPDATE, 
+DROP COLUMN OBS_FLAG, 
+DROP COLUMN CONF_STATUS, 
+DROP COLUMN DATAFLOW;
+
+ALTER TABLE crime_1998_2007
+DROP DATAFLOW;
+
+ALTER TABLE crime_1998_2007
+ADD COLUMN ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST;
+
+-- First Table End Product
+SELECT *
 FROM
-	crime_1998_2007;
+	crime_1998_2007
+ORDER BY TIME_PERIOD;
+    
+SELECT 
+    iccs, geo, SUM(OBS_VALUE) AS total_98_to_07
+FROM
+    crime_1998_2007
+WHERE NOT iccs = 'Total'
+GROUP BY iccs , geo
+ORDER BY iccs, geo;
+    
+
+
+
+    
